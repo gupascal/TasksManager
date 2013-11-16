d99f7ef59e043f242060a9dc93b4351119ea8452
Guillaume P <guillaume.pascal43@laposte.net>
2013-11-16 19:15:04 +0100

    Add task creation action.

--- src/TMS/TasksManagerBundle/Controller/DefaultController.php
+++ src/TMS/TasksManagerBundle/Controller/DefaultController.php
@@ -3,6 +3,8 @@
 namespace TMS\TasksManagerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use TMS\TasksManagerBundle\Entity\Task;
+use TMS\TasksManagerBundle\Form\Type\CreateTaskType;
 
 class DefaultController extends Controller
 {
@@ -28,4 +30,22 @@ class DefaultController extends Controller
 		
         return $this->render('TMSTasksManagerBundle:Default:show.html.twig', array('task' => $task));
     }
+	
+	public function createAction()
+	{
+		$creation_form = $this->createForm(new CreateTaskType(), new Task());
+		
+		$creation_form->handleRequest($this->getRequest());
+		
+		if ($creation_form->isValid())
+		{
+			$task = $creation_form->getData();
+			$task->setUser($this->getUser());
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($task);
+			$em->flush();
+		}
+		
+		return $this->render('TMSTasksManagerBundle:Default:create.html.twig', array('creation_form' => $creation_form->createView()));
+	}
 }
