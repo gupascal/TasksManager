dd13e99af93088e3da5dcaf6fda8011447e79f48
Guillaume P <guillaume.pascal43@laposte.net>
2013-12-13 16:44:35 +0100

    Clean up some code.

--- src/TMS/TasksManagerBundle/Form/Type/CreateTaskType.php
+++ src/TMS/TasksManagerBundle/Form/Type/CreateTaskType.php
@@ -13,10 +13,6 @@ class CreateTaskType extends AbstractType
 				->add('priority', 'number')
 				->add('due_date', 'datetime')
 				->add('description', 'textarea')
-				// ->add('dep_tasks', 'entity', array('class' => 'TMSTasksManagerBundle:Task',
-												// 'property' => 'name',
-												// 'multiple' => true,
-												// /*'expanded' => true*/))
 				->add('submit', 'submit');
     }
 
